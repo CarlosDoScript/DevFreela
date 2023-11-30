9d3a1d2fec27e07311f9e6bcd24ee82b9504103d
Carlos Henrique <carlosmvsep3@hotmail.com>
2023-11-30 20:51:58 -0300

    adicionando o crud de projetos

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -18,8 +18,55 @@ namespace DevFreela.API.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            await Task.Delay(5000);
             return Ok(); 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CreateProjectModel createProject)
+        {
+            if (createProject.Title.Length > 50)
+                return BadRequest();
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if (updateProject.Description.Length > 200)
+                return BadRequest();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPost("{id}/comments")]
+        public async Task<IActionResult> PostComment(int id, [FromBody] CreateCommentModel createComment)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/start")]
+        public async Task<IActionResult> Start(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/finish")]
+        public async Task<IActionResult> Finish(int id)
+        {
+            return NoContent();
         }
     }
 }
