66d66fe631064cdefe0e6f96c652f208c9c1e0ea
Carlos Henrique <carlosmvsep3@hotmail.com>
2023-11-30 20:54:08 -0300

    criando os endpoints do crud de usuarios

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CreateUserModel createUserModel)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = 1 }, createUserModel);
+        }
+
+        [HttpPut("{id}/login")]
+        public async Task<IActionResult> Login(int id, [FromBody] LoginModel login)
+        {
+            return NoContent();
+        }
+    }
+}
