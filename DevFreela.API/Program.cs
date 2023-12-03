da3fa70a90675fbae127fcbbd3aa3eba22335e86
Carlos Henrique <carlosmvsep3@hotmail.com>
2023-12-03 10:50:10 -0300

    Gerando e aplicando as migrations

--- DevFreela.API/Program.cs
+++ DevFreela.API/Program.cs
@@ -1,14 +1,17 @@
 using DevFreela.Application.Services.Implementations;
 using DevFreela.Application.Services.Interfaces;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<DevFreelaDbContext>();
+
+var connectionString = builder.Configuration.GetConnectionString("DevFreelaCs");
+builder.Services.AddDbContext<DevFreelaDbContext>(p => p.UseSqlServer(connectionString));
+
 builder.Services.AddScoped<IProjectService, ProjectService>();
 builder.Services.AddScoped<IUserService, UserService>();
 
---
+++ Infrastructure/Persistence/Migrations/20231203133710_InitialMigration.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevFreela.Infrastructure.Persistence.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Habilidade",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Habilidade", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projeto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdCliente = table.Column<int>(type: "int", nullable: false),
+                    IdFreelancer = table.Column<int>(type: "int", nullable: false),
+                    TotalCost = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StartedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    FinishedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projeto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Projeto_Usuario_IdCliente",
+                        column: x => x.IdCliente,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Projeto_Usuario_IdFreelancer",
+                        column: x => x.IdFreelancer,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "usuarioHabilidade",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdUser = table.Column<int>(type: "int", nullable: false),
+                    IdSkill = table.Column<int>(type: "int", nullable: false),
+                    SkillId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuarioHabilidade", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_usuarioHabilidade_Habilidade_SkillId",
+                        column: x => x.SkillId,
+                        principalTable: "Habilidade",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_usuarioHabilidade_Usuario_IdSkill",
+                        column: x => x.IdSkill,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "comentarioProjeto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdProject = table.Column<int>(type: "int", nullable: false),
+                    IdUser = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_comentarioProjeto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_comentarioProjeto_Projeto_IdProject",
+                        column: x => x.IdProject,
+                        principalTable: "Projeto",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_comentarioProjeto_Usuario_IdUser",
+                        column: x => x.IdUser,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comentarioProjeto_IdProject",
+                table: "comentarioProjeto",
+                column: "IdProject");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comentarioProjeto_IdUser",
+                table: "comentarioProjeto",
+                column: "IdUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projeto_IdCliente",
+                table: "Projeto",
+                column: "IdCliente");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projeto_IdFreelancer",
+                table: "Projeto",
+                column: "IdFreelancer");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_usuarioHabilidade_IdSkill",
+                table: "usuarioHabilidade",
+                column: "IdSkill");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_usuarioHabilidade_SkillId",
+                table: "usuarioHabilidade",
+                column: "SkillId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "comentarioProjeto");
+
+            migrationBuilder.DropTable(
+                name: "usuarioHabilidade");
+
+            migrationBuilder.DropTable(
+                name: "Projeto");
+
+            migrationBuilder.DropTable(
+                name: "Habilidade");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
