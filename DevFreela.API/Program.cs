76194327d664559c8eaee18cf86efe1063a25eaa
Carlos Henrique <carlosmvsep3@hotmail.com>
2023-12-17 18:08:26 -0300

    Gerando migration com os dados de login

--- DevFreela.API/Program.cs
+++ DevFreela.API/Program.cs
@@ -2,6 +2,8 @@ using DevFreela.API.Filters;
 using DevFreela.Application.Commands.CreateProject;
 using DevFreela.Application.Validators;
 using DevFreela.Core.Repositories;
+using DevFreela.Core.Services;
+using DevFreela.Infrastructure.Auth;
 using DevFreela.Infrastructure.Persistence;
 using DevFreela.Infrastructure.Persistence.Repositories;
 using FluentValidation.AspNetCore;
@@ -20,6 +22,7 @@ builder.Services.AddDbContext<DevFreelaDbContext>(p => p.UseSqlServer(connection
 builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<ISkillRepository, SkillRepository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddMediatR(x => x.RegisterServicesFromAssembly(typeof(CreateProjectCommand).Assembly));
 
--- DevFreela.Core/Entities/User.cs
+++ DevFreela.Core/Entities/User.cs
@@ -2,17 +2,19 @@
 {
     public class User : BaseEntity
     {
-        public User(string fullName, string email, DateTime birthDate)
+        public User(string fullName, string email, DateTime birthDate, string password, string role)
         {
             FullName = fullName;
             Email = email;
             BirthDate = birthDate;
             Active = true;
+            Password = password;
+            Role = role;
 
             CreatedAt = DateTime.Now;
             Skills = new List<UserSkill>();
             OwnedProjects = new List<Project>();
-            FreelanceProjects = new List<Project>();
+            FreelanceProjects = new List<Project>();          
         }
 
         public string FullName { get; private set; }
@@ -20,6 +22,8 @@
         public DateTime BirthDate { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public bool Active { get; private set; }
+        public string Password { get; private set; }
+        public string Role { get; private set; }
 
         public List<UserSkill> Skills { get; private set; }
         public List<Project> OwnedProjects { get; private set; }
