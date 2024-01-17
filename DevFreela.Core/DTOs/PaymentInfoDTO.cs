d5b1e773f597edf3012671aa179851e95de97e3c
Carlos Henrique <carlosmvsep3@hotmail.com>
2024-01-16 21:57:40 -0300

    Implementando a interface do Payment service

---
+++ DevFreela.Core/DTOs/PaymentInfoDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace DevFreela.Core.DTOs
+{
+    public record PaymentInfoDTO
+    {
+        public PaymentInfoDTO(int idProject, string creditCardNumber, string cvv, string expiresAt, string fullName, decimal amount)
+        {
+            IdProject = idProject;
+            CreditCardNumber = creditCardNumber;
+            Cvv = cvv;
+            ExpiresAt = expiresAt;
+            FullName = fullName;
+            Amount = amount;
+        }
+
+        public int IdProject { get; init; }
+        public string CreditCardNumber { get; init; }
+        public string Cvv { get; init; }
+        public string ExpiresAt { get; init; }
+        public string FullName { get; init; }
+        public decimal Amount { get; init; }
+    }
+}
